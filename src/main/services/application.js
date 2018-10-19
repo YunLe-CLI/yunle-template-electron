2514765a5abeb5a171b1ba771f642fa4cf96569e
hexiao-o <460559761@qq.com>
2018-10-20 00:24:20 +0800

    add eslint

--- src/main/services/application.js
+++ src/main/services/application.js
@@ -1,6 +1,25 @@
 import { create, getPath } from './window';
+import download from '../utils/StreamDownload';
 
 export function init() {
   const win = create({ width: 800, height: 600 });
   win.loadURL(getPath());
+
+  // 调用下载
+  download.downloadFile("https://class100-ppt.oss-cn-shanghai.aliyuncs.com/TESTDD/ui/teaching/latest/build.zip", "./", (arg, percentage) => {
+    if (arg === "progress") {
+      console.log(111111, percentage)
+      // 显示进度
+    } else if (arg === "finished") {
+      // 通知完成
+    }
+  })
+  download.downloadFile("https://class100-ppt.oss-cn-shanghai.aliyuncs.com/TESTDD/ui/teaching/latest/build.zip", "./", (arg, percentage) => {
+    if (arg === "progress") {
+        console.log(222222, percentage)
+        // 显示进度
+    } else if (arg === "finished") {
+        // 通知完成
+    }
+  })
 }
