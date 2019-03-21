6b81e15297ddc19a748325cdeb00e2c8e32a84f9
hexiao-o <460559761@qq.com>
2019-03-21 14:29:04 +0800

    更新

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ export default function (webpackConfig, { webpack }) {
   webpackConfig.output.path = join(cwd, './app/dist/main');
   webpackConfig.target = 'electron';
   webpackConfig.externals = (context, request, callback) => {
-    if (request.charAt(0) === '.' || request.charAt(0) === '@') {
+    if (request.charAt(0) === '.' || request.indexOf('@babel') > -1) {
       callback(null, false)
     } else {
       callback(null, `require("${request}")`);
